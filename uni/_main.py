1154b1d1daa3871eaf268afcfea7980ff9d66600
Yoda <im.yod4@gmail.com>
2022-10-21 21:19:08 +0330

    some minor changes

--- uni/_main.py
+++ uni/_main.py
@@ -1,10 +1,11 @@
 from luna.match import is_matched_parentheses
-from ._expressin_tree import ExpressionTree
+from ._expression_tree import ExpressionTree
 
 def expression(ex):
     if not is_matched_parentheses(ex):
         raise ValueError('Parentheses does not match')
-    ex = ''.join(ex.split())
+    ex = '('.join(ex.split())
+    ex += ')'
     return _build_expression_tree(ex)
 
 def _build_expression_tree(token):
