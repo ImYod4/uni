a5a71134f58d458ac08b27e42659414d82cab062
Yoda <im.yod4@gmail.com>
2022-10-21 21:04:06 +0330

    build expression tree

---
+++ uni/_main.py
@@ -0,0 +1,22 @@
+from luna.match import is_matched_parentheses
+from ._expressin_tree import ExpressionTree
+
+def expression(ex):
+    if not is_matched_parentheses(ex):
+        raise ValueError('Parentheses does not match')
+    ex = ''.join(ex.split())
+    return _build_expression_tree(ex)
+
+def _build_expression_tree(token):
+    S = []
+    for t in token:
+        if t in '+-*/=':
+            S.append(t)
+        elif not t in '()':
+            S.append(ExpressionTree(t))
+        elif t == ')':
+            right = S.pop()
+            op = S.pop()
+            left = S.pop()
+            S.append(ExpressionTree(op, left, right))
+    return S.pop()
